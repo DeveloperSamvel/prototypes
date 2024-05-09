45696a1777000db33e460e8c327e93b01eef8dba
Samvel Vardanyan <129245937+DeveloperSamvel@users.noreply.github.com>
2024-05-09 14:31:28 +0400

    fixed human object

--- human.js
+++ human.js
@@ -2,7 +2,7 @@ const human = Object.create(null);
 human.name = "Sam";
 human.age = 27;
 human.city = "Masis";
-human.say = function () {
+human.greeting = function () {
   return `Welcome ${this.name}`;
 }
 
